80039c6f97f1ba33b094c1c1e3e0e0411e0deabf
toob <obdrzalek.tom@gmail.com>
2024-08-05 19:15:53 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         }}
       >
         <GridItem area="nav" bg="coral">
-          Nav
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="gold">
